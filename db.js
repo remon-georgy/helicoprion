64209372b331625cf78fa49a23bbf62ba503669d
Remon Georgy <remon.sherin@gmail.com>
2016-08-30 20:05:56 -0400

    RethinkDB + react-native-web modules

--- db.js
+++ db.js
@@ -1,14 +1,27 @@
-var data = require('./data');
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
+var movements = require('./data').movements;
+var workouts = require('./data').workouts;
+var r = require('rethinkdb');
+var _ = require('lodash');
+var movementsArray = _.values(movements);
+var workoutsArray = _.values(workouts);
+var p = r.connect({db: 'wodmeup'});
 
-var url = 'mongodb://localhost:27017/helicoprion';
-MongoClient.connect(url, function(err, db) {
-  var workoutsCollection = db.collection('workouts');
-  workoutsCollection.insertMany(data.workouts, function(err, result) {
-    console.log(err);
-    console.log(result);
+p.then(function(conn) {
+  // Movements
+  r.tableCreate('movements').run(conn, function() {
+    r.table('movements').insert(movementsArray).run(conn, function(err, result) {
+      console.log(err);
+      console.log(result);
+    });
   });
   
-  db.close();
+  // Workouts
+  r.tableCreate('workouts').run(conn, function() {
+    r.table('workouts').insert(workoutsArray).run(conn, function(err, result) {
+      console.log(err);
+      console.log(result);
+    });
+  });
+}).error(function(err){
+  console.log(err);
 });
