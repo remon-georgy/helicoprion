a89cbb635230d1252a05944aff221785710417ab
Remon Georgy <remon.sherin@gmail.com>
2016-12-03 04:08:20 -0800

    drop tables before creating new ones

--- table-creation.js
+++ table-creation.js
@@ -11,39 +11,46 @@ const r = require('rethinkdb');
 const _ = require('lodash');
 const movementsArray = _.values(movements);
 const workoutsArray = _.values(workouts);
-const p = r.connect({db: 'wodmeup'});
+const p = r.connect();
 
 p.then(function(conn) {
-  // Movements
-  r.tableCreate('movements').run(conn, function() {
-    r.table('movements').insert(movementsArray).run(conn, function(err, result) {
-      console.log(err);
-      console.log(result);
-    });
-  });
-  
-  // Workouts
-  r.tableCreate('workouts').run(conn, function() {
-    r.table('workouts').insert(workoutsArray).run(conn, function(err, result) {
-      console.log(err);
-      console.log(result);
-    });
-  });
-  
-  r.tableCreate('equipments').run(conn, function() {
-    r.table('equipments').insert(equipments).run(conn, function(err, result) {
-      console.log(err);
-      console.log(result);
-    });
-  });
-  
-  r.tableCreate('tags').run(conn, function() {
-    r.table('tags').insert(tags).run(conn, function(err, result) {
-      console.log(err);
-      console.log(result);
-    });
-  });
+  r.dbDrop('wodmeup').run(conn, function() {
+    r.dbCreate('wodmeup').run(conn, function() {
+      conn.use('wodmeup');
 
+      // Movements
+      r.tableCreate('movements').run(conn, function() {
+        r.table('movements').insert(movementsArray).run(conn, function(err, result) {
+          console.log(err);
+          console.log(result);
+        });
+      });
+
+      // Workouts
+      r.tableCreate('workouts').run(conn, function() {
+        r.table('workouts').insert(workoutsArray).run(conn, function(err, result) {
+          console.log(err);
+          console.log(result);
+        });
+      });
+
+      r.tableCreate('equipments').run(conn, function() {
+        r.table('equipments').insert(equipments).run(conn, function(err, result) {
+          console.log(err);
+          console.log(result);
+        });
+      });
+
+      r.tableCreate('tags').run(conn, function() {
+        r.table('tags').insert(tags).run(conn, function(err, result) {
+          console.log(err);
+          console.log(result);
+        });
+      });
+      return;
+    });
+  });
+  
 }).error(function(err){
   console.log(err);
 });
